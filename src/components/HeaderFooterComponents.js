e6f985b411ad697a9865d4980e07a86967f2bbe0
mahmoud mheisen <mahmoudmheisen91@gmail.com>
2020-03-24 04:47:19 +0200

    add basic boilerplate

---
+++ src/components/HeaderFooterComponents.js
@@ -0,0 +1,9 @@
+"use strict";
+
+let HeaderComp = props => {
+  return <header>{props.text}</header>;
+};
+
+let FooterComp = props => {
+  return <footer>&copy; 2020 Mahmoud Mheisen</footer>;
+};
---
+++ src/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+function App() {
+  return (
+    <div>
+      <HeaderComp text="FCC: Front End Libraries - Project 1, Project Name" />
+
+      <FooterComp />
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
